1a38a37439a3f623c3807e0b6d9e96702bf2360c
fgomezeng <fernando.gomez@utp.edu.co>
2026-05-27 10:43:12 -0500

    feat: add function to handle cart item additions

--- guitarla/src/App.jsx
+++ guitarla/src/App.jsx
@@ -5,6 +5,20 @@ import { db } from "./data/db";
 
 function App() {
   const [data, setData] = useState(db);
+  const [cart, setCart] = useState([]);
+
+  function addToCart(item) {
+    const itemExists = cart.findIndex((guitar) => guitar.id === item.id);
+    if (itemExists >= 0) {
+      //existe en el carrito
+      const updatedCart = [...cart];
+      updatedCart[itemExists].quantity++;
+      setCart(updatedCart);
+    } else {
+      item.quantity = 1;
+      setCart([...cart, item]);
+    }
+  }
 
   return (
     <>
@@ -15,7 +29,11 @@ function App() {
 
         <div className="row mt-5">
           {data.map((guitar) => (
-            <Guitar key={guitar.id} guitar={guitar} />
+            <Guitar
+              key={guitar.id}
+              guitar={guitar}
+              addToCart={addToCart}
+            />
           ))}
         </div>
       </main>
