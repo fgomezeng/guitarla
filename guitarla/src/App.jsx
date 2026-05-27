e26c71f3ec6fa889504e3f162e874a8126088b4d
fgomezeng <fernando.gomez@utp.edu.co>
2026-05-27 12:28:09 -0500

    feat: implement shopping cart management functionality

--- guitarla/src/App.jsx
+++ guitarla/src/App.jsx
@@ -6,11 +6,13 @@ import { db } from "./data/db";
 function App() {
   const [data, setData] = useState(db);
   const [cart, setCart] = useState([]);
+  const MAX_ITEMS = 5;
+  const MIN_ITEMS = 1;
 
   function addToCart(item) {
     const itemExists = cart.findIndex((guitar) => guitar.id === item.id);
-    if (itemExists >= 0) {
-      //existe en el carrito
+    if (itemExists >= 0) { //existe en el carrito
+      if(cart[itemExists].quantity >= MAX_ITEMS) return
       const updatedCart = [...cart];
       updatedCart[itemExists].quantity++;
       setCart(updatedCart);
@@ -20,20 +22,50 @@ function App() {
     }
   }
 
+  function removeFromCart(id) {
+    setCart(prevCart => prevCart.filter(guitar => guitar.id !== id));
+  }
+
+  function increaseQuantity(id) {
+    const updatedCart = cart.map((item) => {
+      if(item.id === id && item.quantity < MAX_ITEMS){
+        return {
+          ...item, 
+          quantity: item.quantity + 1
+        }
+      }
+      return item;
+    })
+    setCart(updatedCart);
+  }
+
+  function decreaseQuantity(id) {
+    const updatedCart = cart.map((item) => {
+      if(item.id === id && item.quantity > MIN_ITEMS) {
+        return {
+          ...item,
+          quantity: item.quantity - 1
+        }
+      }
+      return item;
+    })
+    setCart(updatedCart);
+  }
+
+  function clearCart() {
+    setCart([]);
+  }
+
   return (
     <>
-      <Header />
+      <Header cart={cart} removeFromCart={removeFromCart} increaseQuantity={increaseQuantity} decreaseQuantity={decreaseQuantity} clearCart={clearCart}/>
 
       <main className="container-xl mt-5">
         <h2 className="text-center">Nuestra Colección</h2>
 
         <div className="row mt-5">
           {data.map((guitar) => (
-            <Guitar
-              key={guitar.id}
-              guitar={guitar}
-              addToCart={addToCart}
-            />
+            <Guitar key={guitar.id} guitar={guitar} addToCart={addToCart} />
           ))}
         </div>
       </main>
