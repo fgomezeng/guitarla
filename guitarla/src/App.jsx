26bc3d91da8cd2189a0de257d29e8721e3ee4ddb
fgomezeng <fernando.gomez@utp.edu.co>
2026-05-27 12:55:37 -0500

    feat: persist shopping cart data using localStorage

--- guitarla/src/App.jsx
+++ guitarla/src/App.jsx
@@ -1,18 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Header from "./components/Header";
 import Guitar from "./components/Guitar";
 import { db } from "./data/db";
 
 function App() {
+
+  const initialCart = () => {
+    const localStorageCart = localStorage.getItem('cart')
+    return localStorageCart ? JSON.parse(localStorageCart) : []
+  }
+
   const [data, setData] = useState(db);
-  const [cart, setCart] = useState([]);
+  const [cart, setCart] = useState(initialCart);
+
   const MAX_ITEMS = 5;
   const MIN_ITEMS = 1;
 
+  useEffect(() => {
+    localStorage.setItem("cart", JSON.stringify(cart));
+  }, [cart]);
+
   function addToCart(item) {
     const itemExists = cart.findIndex((guitar) => guitar.id === item.id);
-    if (itemExists >= 0) { //existe en el carrito
-      if(cart[itemExists].quantity >= MAX_ITEMS) return
+    if (itemExists >= 0) {
+      //existe en el carrito
+      if (cart[itemExists].quantity >= MAX_ITEMS) return;
       const updatedCart = [...cart];
       updatedCart[itemExists].quantity++;
       setCart(updatedCart);
@@ -23,32 +35,32 @@ function App() {
   }
 
   function removeFromCart(id) {
-    setCart(prevCart => prevCart.filter(guitar => guitar.id !== id));
+    setCart((prevCart) => prevCart.filter((guitar) => guitar.id !== id));
   }
 
   function increaseQuantity(id) {
     const updatedCart = cart.map((item) => {
-      if(item.id === id && item.quantity < MAX_ITEMS){
+      if (item.id === id && item.quantity < MAX_ITEMS) {
         return {
-          ...item, 
-          quantity: item.quantity + 1
-        }
+          ...item,
+          quantity: item.quantity + 1,
+        };
       }
       return item;
-    })
+    });
     setCart(updatedCart);
   }
 
   function decreaseQuantity(id) {
     const updatedCart = cart.map((item) => {
-      if(item.id === id && item.quantity > MIN_ITEMS) {
+      if (item.id === id && item.quantity > MIN_ITEMS) {
         return {
           ...item,
-          quantity: item.quantity - 1
-        }
+          quantity: item.quantity - 1,
+        };
       }
       return item;
-    })
+    });
     setCart(updatedCart);
   }
 
@@ -58,7 +70,13 @@ function App() {
 
   return (
     <>
-      <Header cart={cart} removeFromCart={removeFromCart} increaseQuantity={increaseQuantity} decreaseQuantity={decreaseQuantity} clearCart={clearCart}/>
+      <Header
+        cart={cart}
+        removeFromCart={removeFromCart}
+        increaseQuantity={increaseQuantity}
+        decreaseQuantity={decreaseQuantity}
+        clearCart={clearCart}
+      />
 
       <main className="container-xl mt-5">
         <h2 className="text-center">Nuestra Colección</h2>
